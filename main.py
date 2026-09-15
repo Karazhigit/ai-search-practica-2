9b8710c602c101e3aa07254c8a5ab0e64b523033
Karazhigit <alisherkarazhigit77@gmail.com>
2026-09-15 20:35:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+graph = {
+    "Gate": [("Library", 2), ("Cafe", 1), ("Hall", 4)],
+    "Library": [("Classroom", 4), ("Study Zone", 2)],
+    "Cafe": [("Study Zone", 5), ("Robotics Lab", 2)],
+    "Hall": [("Classroom", 1), ("Robotics Lab", 6)],
+    "Classroom": [("AI Lab", 8)],
+    "Study Zone": [("Robotics Lab", 1), ("AI Lab", 5)],
+    "Robotics Lab": [("AI Lab", 4)],
+    "AI Lab": []
+}
